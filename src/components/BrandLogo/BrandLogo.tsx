9a4afeaf87f000fe55db7d0aa54f638de7335125
David Oliveira <davidstna21@gmail.com>
2024-07-12 06:03:16 -0300

    add BrandLogo component

---
+++ src/components/BrandLogo/BrandLogo.tsx
@@ -0,0 +1,7 @@
+import logo from '../../assets/quero_logo_roxo_v.webp';
+
+const BrandLogo = () => {
+  return <img src={logo} alt="brand logo" />;
+};
+
+export { BrandLogo };
---
+++ src/components/BrandLogo/index.ts
@@ -0,0 +1,1 @@
+export { BrandLogo } from './BrandLogo';
