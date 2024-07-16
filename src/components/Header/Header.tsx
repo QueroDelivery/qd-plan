6184a9b91f551d46ef874667c6b119d964f0905e
David Oliveira <davidstna21@gmail.com>
2024-07-16 00:11:08 -0300

    add background color and border-shadow to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { MenuBurguerButton } from './components/MenuBurguerButton';
 
 const Header = () => {
   return (
-    <div className="block lg:hidden z-40 sticky top-0">
+    <div className="block lg:hidden z-40 sticky top-0 bg-white shadow-sm">
       <div className="flex items-center justify-between min-w-[336px] w-full h-20 px-5">
         <div className="w-[100px] sm:w-[120px]">
           <BrandLogo />
