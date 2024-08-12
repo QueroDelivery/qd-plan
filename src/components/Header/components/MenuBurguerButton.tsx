617b9d7e8c4bd55750fd57f59934ea735285bd28
David Oliveira <davidstna21@gmail.com>
2024-08-12 09:31:21 -0300

    add transition to MenuBurguerButton on hover

--- src/components/Header/components/MenuBurguerButton.tsx
+++ src/components/Header/components/MenuBurguerButton.tsx
@@ -10,7 +10,7 @@ const MenuBurguerButton = ({ isOpen, toggleOpen }: MenuBurguerButton) => {
   return (
     <button
       onClick={() => toggleOpen((value) => !value)}
-      className="rounded-full flex items-center justify-center w-12 h-12 hover:bg-purple-500 hover:bg-opacity-20"
+      className="rounded-full flex items-center justify-center w-12 h-12 hover:bg-purple-500 hover:bg-opacity-20 transition-all"
     >
       {isOpen ? <TfiClose size={22} /> : <RxHamburgerMenu size={25} />}
     </button>
