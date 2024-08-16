8951e1316654729ab37386b1ad38295adc376ca9
David Oliveira <davidstna21@gmail.com>
2024-08-16 14:56:04 -0300

    add wraping div around the main tag and add h1 title to App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,14 @@ function App() {
       <Header />
       <div className="grid grid-cols-only-content lg:grid-cols-sidebar-content">
         <Sidebar />
-        <main className="container py-12 mx-auto min-w-[336px]">
-          <PlanAcaoTable />
-        </main>
+        <div className="flex flex-col">
+          <h1 className="text-xl md:text-2xl font-bold text-purple-500 px-6 py-4 border-b border-gray-300/60">
+            Metas e ações
+          </h1>
+          <main className="container py-8 mx-auto min-w-[336px]">
+            <PlanAcaoTable />
+          </main>
+        </div>
       </div>
     </>
   );
