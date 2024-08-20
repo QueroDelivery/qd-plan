240c3759ddab697fc0eb2bd837db6f5ab7401a18
David Oliveira <davidstna21@gmail.com>
2024-08-20 14:16:13 -0300

    changes breakpoint to hide the Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="block lg:hidden z-40 sticky top-0 bg-white shadow-sm">
+    <div className="block xl:hidden z-40 sticky top-0 bg-white shadow-sm">
       <div className="flex items-center justify-between min-w-[336px] w-full h-20 px-5">
         <div className="w-[100px] sm:w-[120px]">
           <BrandLogo />
