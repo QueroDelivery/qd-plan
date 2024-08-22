4390454353f872d0c4add075c4fec96fbac7e26e
David Oliveira <davidstna21@gmail.com>
2024-08-22 08:49:47 -0300

    display MetasMunicipio component, add overflow-x-hidden to wraping div and changes grid breakpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header';
+import { MetasMunicipio } from './components/MetasMunicipio';
 import { PlanAcaoTable } from './components/PlanAcaoTable';
 import { Sidebar } from './components/Sidebar';
 
@@ -6,12 +7,13 @@ function App() {
   return (
     <>
       <Header />
-      <div className="grid grid-cols-only-content lg:grid-cols-sidebar-content">
+      <div className="grid grid-cols-only-content xl:grid-cols-sidebar-content">
         <Sidebar />
-        <div className="flex flex-col">
+        <div className="flex flex-col overflow-x-hidden">
           <h1 className="text-xl md:text-2xl font-bold text-purple-500 px-6 py-4 border-b border-gray-300/60">
             Metas e ações
           </h1>
+          <MetasMunicipio />
           <main className="container py-8 mx-auto min-w-[336px]">
             <PlanAcaoTable />
           </main>
