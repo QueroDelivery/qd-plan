7d9674793df1ae32a873d0871cd05d88dbc6c38a
David Oliveira <davidstna21@gmail.com>
2024-09-03 16:57:21 -0300

    checks for data first and handle error in MetasMunicipio component

--- src/components/MetasMunicipio/MetasMunicipio.tsx
+++ src/components/MetasMunicipio/MetasMunicipio.tsx
@@ -2,15 +2,11 @@ import 'react-circular-progressbar/dist/styles.css';
 import { ImSpinner8 } from 'react-icons/im';
 import { DashboardMetas } from './components/DashboardMetas';
 import useMetas from 'src/hooks/useMetas';
+import { VscError } from 'react-icons/vsc';
+import { Button } from 'src/components/ui/button';
 
 const MetasMunicipio = ({ municipioId }: { municipioId: string }) => {
   const metasQuery = useMetas(municipioId);
-
-  const loading = (
-    <div className="col-span-full justify-self-center">
-      <ImSpinner8 className="animate-spin text-purple-500" size={24} />
-    </div>
-  );
 
   const content = (
     <>
@@ -41,11 +37,38 @@ const MetasMunicipio = ({ municipioId }: { municipioId: string }) => {
     </>
   );
 
-  return (
-    <div className="grid sm:grid-cols-1 min-[950px]:grid-cols-2 min-[1720px]:grid-cols-3 gap-5">
-      {metasQuery.isLoading ? loading : content}
+  const loading = (
+    <div className="flex justify-center">
+      <ImSpinner8 className="animate-spin text-purple-500" size={24} />
     </div>
   );
+
+  if (metasQuery.data) {
+    return (
+      <div className="grid sm:grid-cols-1 min-[950px]:grid-cols-2 min-[1720px]:grid-cols-3 gap-5">
+        {content}
+      </div>
+    );
+  }
+
+  if (metasQuery.error) {
+    return (
+      <div className="flex flex-col justify-center items-center gap-3">
+        <div className="flex items-center">
+          <VscError size={24} className="mr-3" color="red" />
+          <p className="text-gray-700">
+            Erro ao exibir metas: {metasQuery.error.message}. Tente novamente em
+            instantes.
+          </p>
+        </div>
+        <Button variant="destructive" onClick={() => metasQuery.refetch()}>
+          Tentar novamente
+        </Button>
+      </div>
+    );
+  }
+
+  return loading;
 };
 
 export { MetasMunicipio };
