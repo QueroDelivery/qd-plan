f71b6fbf8eba04b32e3360a948a5bf70aab0661c
David Oliveira <davidstna21@gmail.com>
2024-09-04 09:22:20 -0300

    change colors and add pointer cursor on hover

--- src/styles/selectStyles.ts
+++ src/styles/selectStyles.ts
@@ -9,6 +9,7 @@ export const singleSelectStyles: StylesConfig<{
     border: '1px solid rgba(209, 213, 219, 1)',
     boxShadow: 'none',
     ':hover': {
+      cursor: 'pointer',
       border: '1px solid rgba(209, 213, 219, 1)',
       boxShadow:
         '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
@@ -16,7 +17,7 @@ export const singleSelectStyles: StylesConfig<{
   }),
   option: (baseStyles, { isDisabled, isSelected, isFocused }) => ({
     ...baseStyles,
-    color: '#374151',
+    color: '#4b5563',
     backgroundColor: isDisabled
       ? undefined
       : isSelected
@@ -32,5 +33,17 @@ export const singleSelectStyles: StylesConfig<{
           : 'rgba(168, 85, 247, 0.6)'
         : undefined,
     },
+  }),
+  input: (baseStyles) => ({
+    ...baseStyles,
+    color: '#6b7280',
+  }),
+  placeholder: (baseStyles) => ({
+    ...baseStyles,
+    color: '#6b7280',
+  }),
+  singleValue: (baseStyles) => ({
+    ...baseStyles,
+    color: '#6b7280',
   }),
 };
