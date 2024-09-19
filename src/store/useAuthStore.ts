dce0e1741eeca31d161798fe765ecc4601c2b1fa
David Oliveira <davidstna21@gmail.com>
2024-09-19 13:54:42 -0300

    add auth store

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+
+interface User {
+  _id: string;
+  nome: string;
+  celular: string;
+  email: string;
+  municipioIds: string[];
+  token: string;
+}
+
+type AuthState = {
+  user: User | null;
+};
+
+type AuthActions = {
+  setUser: (userData: User) => void;
+  clearUser: () => void;
+};
+
+const useAuthStore = create<AuthState & AuthActions>((set) => ({
+  user: null,
+  setUser: (userData) => set({ user: userData }),
+  clearUser: () => set({ user: null }),
+}));
+
+export default useAuthStore;
