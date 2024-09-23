2a9b50e0b1d177b4aa39b2ac14d20d0a020fcc09
David Oliveira <davidstna21@gmail.com>
2024-09-23 10:15:20 -0300

    add interceptor to handle error and redirect user on status code 401

--- src/axiosClient/index.ts
+++ src/axiosClient/index.ts
@@ -1,4 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import Cookies from 'js-cookie';
+import useAuthStore from 'src/store/useAuthStore';
+import { redirect } from 'react-router-dom';
 
 const httpClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -7,13 +10,23 @@ const httpClient = axios.create({
   },
 });
 
-// httpClient.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response) {
-//       const statusCode = error.response.status;
-//     }
-//   }
-// );
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response) {
+      const statusCode = error.response.status;
+
+      switch (statusCode) {
+        case 401: {
+          Cookies.remove('qd-token');
+          useAuthStore.getState().clearUser();
+          redirect('/login');
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default httpClient;
