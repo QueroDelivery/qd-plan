47d8b0c29c93873995401630ce003ad9808e7d8a
David Oliveira <davidstna21@gmail.com>
2024-09-24 17:25:26 -0300

    add reusable Heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { cn } from 'src/lib/utils';
+
+type HeadingProps = {
+  className?: string;
+} & React.HTMLAttributes<HTMLHeadingElement>;
+
+const Heading = ({ children, className }: HeadingProps) => {
+  return (
+    <h1
+      className={cn(
+        'text-xl md:text-2xl font-bold text-purple-500 px-6 py-4',
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+};
+
+export { Heading };
