ab5c0bc650a9d30ffd9309fb84d5c0d3030450dc
David Oliveira <davidstna21@gmail.com>
2024-09-26 09:57:05 -0300

    add property size

--- src/LoadingSpinner/LoadingSpinner.tsx
+++ src/LoadingSpinner/LoadingSpinner.tsx
@@ -1,9 +1,9 @@
 import { ImSpinner8 } from 'react-icons/im';
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ size = 24 }: { size?: number }) => {
   return (
     <div className="flex justify-center items-center">
-      <ImSpinner8 className="animate-spin text-purple-500" size={24} />
+      <ImSpinner8 className="animate-spin text-purple-500" size={size} />
     </div>
   );
 };
