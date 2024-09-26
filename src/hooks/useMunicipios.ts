c25258283692ef6434b7b2ff27051a8c2684067b
David Oliveira <davidstna21@gmail.com>
2024-09-26 09:56:13 -0300

    add useMunicipios hook

---
+++ src/hooks/useMunicipios.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import httpClient from 'src/axiosClient';
+
+type MunicipiosResponse = {
+  r: boolean;
+  data: TMunicipio[];
+};
+
+type TMunicipio = {
+  municipioId: string;
+  nome: string;
+  uf: string;
+};
+
+const getAllMunicipios = async (): Promise<TMunicipio[]> => {
+  const { data: response } = await httpClient.get<MunicipiosResponse>(
+    'getAllMunicipios'
+  );
+  return response.data;
+};
+
+export default function useMunicipios() {
+  return useQuery({
+    queryKey: ['municipios'],
+    queryFn: getAllMunicipios,
+    refetchOnWindowFocus: false,
+  });
+}
