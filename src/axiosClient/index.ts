cd826238a003707850549b611d8ece1e131768e4
David Oliveira <davidstna21@gmail.com>
2024-10-04 14:33:51 -0300

    add setAuthorizationToken fn to set bearer token and add type guard for errors in http responses

--- src/axiosClient/index.ts
+++ src/axiosClient/index.ts
@@ -1,29 +1,29 @@
 import axios, { AxiosError } from 'axios';
 import Cookies from 'js-cookie';
-import useAuthStore from 'src/store/useAuthStore';
-import { redirect } from 'react-router-dom';
+import { AUTH_TOKEN_KEY } from 'src/constants/auth';
 
 const httpClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    Authorization: import.meta.env.VITE_AUTHORIZATION_TOKEN,
-  },
 });
+
+export const setAuthorizationToken = (token: string) => {
+  httpClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
 
 httpClient.interceptors.response.use(
   (response) => response,
-  (error: AxiosError) => {
-    if (error.response) {
-      const statusCode = error.response.status;
+  (error) => {
+    if (error instanceof AxiosError)
+      if (error.response) {
+        const statusCode = error.response.status;
 
-      switch (statusCode) {
-        case 401: {
-          Cookies.remove('qd-token');
-          useAuthStore.getState().clearUser();
-          redirect('/login');
+        switch (statusCode) {
+          case 401: {
+            Cookies.remove(AUTH_TOKEN_KEY);
+            window.location.reload();
+          }
         }
       }
-    }
 
     return Promise.reject(error);
   }
