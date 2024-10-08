94f77b1e300aa4599f885ef80d1ebd5ecc4fc8bf
David Oliveira <davidstna21@gmail.com>
2024-10-08 09:16:31 -0300

    removes unnecessary styles

--- src/components/ScreenLoader/ScreenLoader.tsx
+++ src/components/ScreenLoader/ScreenLoader.tsx
@@ -3,9 +3,7 @@ import logo from 'src/assets/quero_logo_branco_v.webp';
 const ScreenLoader = () => {
   return (
     <div className="fixed inset-0 bg-purple-500 flex justify-center items-center">
-      <div className="space-y-6 text-center">
-        <img src={logo} alt="logo" width={200} />
-      </div>
+      <img src={logo} alt="logo" width={200} />
     </div>
   );
 };
